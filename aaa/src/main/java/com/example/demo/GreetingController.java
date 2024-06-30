c42fa6b02aeef3b7467c48315fdca4b3ae80bb0f
Mateusz <matrys000@utp.edu.pl>
2024-06-30 09:03:33 +0200

    Kadrometer V.2.1

--- aaa/src/main/java/com/example/demo/GreetingController.java
+++ aaa/src/main/java/com/example/demo/GreetingController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController { // nazwa klasy niezgodna ze standardami, powinna być PascalCase
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s!"; // stała powinna być pisana wielkimi literami
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(String.format(template, name));
+        return new Greeting(String.format(template, name)); // zamiast bezpośrednio zwracać wynik, moglibyśmy użyć serwisu, ale nie jest to tutaj zrobione
     }
 
 }
